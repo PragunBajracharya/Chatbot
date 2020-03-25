c827fee63a7e74141f7159baf2ab5c55f1129703
Pragun Bajracharya <pragunbaj99@gmail.com>
2020-03-25 20:55:53 +0545

    Start of tensor flow

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,12 @@
-# Building a ChatBot with Deep NLP
-
-
-
-# Importing the libraries
 import numpy as np
 import tensorflow as tf
 import re
 import time
+
+
+
+########## PART 1 - DATA PREPROCESSING ##########
+
 
 
 # Importing the dataset
@@ -49,9 +49,7 @@ def clean_text(text):
     text = re.sub(r"\'d", " would", text)
     text = re.sub(r"won't", "will not", text)
     text = re.sub(r"can't", "cannot", text)
-    text = re.sub(r"n't", " not", text)
-    text = re.sub(r"\'kay", "okay", text)
-    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,*]", "", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
     return text
 
 # Cleaning the questions
@@ -87,7 +85,6 @@ for word, count in word2count.items():
     if count >= threshold_questions:
         questionswords2int[word] = word_number
         word_number += 1
-        
 threshold_answers = 20
 answerswords2int = {}
 word_number = 0
@@ -95,25 +92,24 @@ for word, count in word2count.items():
     if count >= threshold_answers:
         answerswords2int[word] = word_number
         word_number += 1
-        
-#Adding the last tokens to these two dictionaries
+
+# Adding the last tokens to these two dictionaries
 tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
 for token in tokens:
     questionswords2int[token] = len(questionswords2int) + 1
-
 for token in tokens:
-    answerswords2int[token] = len(answerswords2int) + 1   
+    answerswords2int[token] = len(answerswords2int) + 1
 
-#Creating the inverse dictionary of the answerswords2int dictionary
+# Creating the inverse dictionary of the answerswords2int dictionary
 answersints2word = {w_i: w for w, w_i in answerswords2int.items()}
 
-#Adding the End of String token to the end of every answer
+# Adding the End Of String token to the end of every answer
 for i in range(len(clean_answers)):
-    clean_answers[i] +=  ' <EOS>'
-    
-#Translating all the questions and the answers into integer
-# and Replacing all the words that were filtered out by <OUT>
-questions_to_int = []
+    clean_answers[i] += ' <EOS>'
+
+# Translating all the questions and the answers into integers
+# and Replacing all the words that were filtered out by <OUT> 
+questions_into_int = []
 for question in clean_questions:
     ints = []
     for word in question.split():
@@ -121,9 +117,8 @@ for question in clean_questions:
             ints.append(questionswords2int['<OUT>'])
         else:
             ints.append(questionswords2int[word])
-    questions_to_int.append(ints)
-    
-answers_to_int = []
+    questions_into_int.append(ints)
+answers_into_int = []
 for answer in clean_answers:
     ints = []
     for word in answer.split():
@@ -131,40 +126,34 @@ for answer in clean_answers:
             ints.append(answerswords2int['<OUT>'])
         else:
             ints.append(answerswords2int[word])
-    answers_to_int.append(ints)
-        
-    
+    answers_into_int.append(ints)
 
-#Building The SEQ2SEQ Model
+# Sorting questions and answers by the length of questions
+sorted_clean_questions = []
+sorted_clean_answers = []
+for length in range(1, 25 + 1):
+    for i in enumerate(questions_into_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_into_int[i[0]])
+            sorted_clean_answers.append(answers_into_int[i[0]])
 
+
+
+########## PART 2 - BUILDING THE SEQ2SEQ MODEL ##########
+
+
+
+# Creating placeholders for the inputs and the targets
 def model_inputs():
     inputs = tf.placeholder(tf.int32, [None, None], name = 'input')
     targets = tf.placeholder(tf.int32, [None, None], name = 'target')
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    lr = tf.placeholder(tf.float32, name = 'learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name = 'keep_prob')
+    return inputs, targets, lr, keep_prob
+
+# Preprocessing the targets
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0], [batch_size, -1], [1,1])
+    preprocessed_targets = tf.concat([left_side, right_side], 1)
+    return preprocessed_targets
